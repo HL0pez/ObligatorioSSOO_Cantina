00911e2184e17ddc88bdb2e1cc804ade3a5eea19
Hernan Lopez <hlhernano@gmail.com>
2026-06-18 08:25:56 -0300

    Estructura

--- simulacion/src/main/java/cantina/ucu/Implementaciones/Cafetera.java
+++ simulacion/src/main/java/cantina/ucu/Implementaciones/Cafetera.java
@@ -1,6 +1,6 @@
 package cantina.ucu.Implementaciones;
 
-import java.util.PriorityQueue;
+import java.util.LinkedList;
 import java.util.Queue;
 
 import cantina.ucu.Interfaces.IPedido;
@@ -8,11 +8,12 @@ import cantina.ucu.Interfaces.IRecursoCompartido;
 
 public class Cafetera implements IRecursoCompartido {
 
-    PriorityQueue<IPedido> pendientes = null;
+    Queue<IPedido> pendientes = null;
 
     public Cafetera(){
-        this.pendientes = new PriorityQueue<>();
+        this.pendientes = new LinkedList<>();
     }
+
     @Override
     public void atenderPedido(IPedido pedido) {
         // TODO Auto-generated method stub
