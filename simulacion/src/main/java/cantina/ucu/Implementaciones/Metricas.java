7cf90c79438e0c2fb9e451d10971e893175ef1b8
MiliGarciaDaSilva <mariamilagrosgarciadasilva@gmail.com>
2026-06-23 10:24:50 -0300

    singleton metricas

--- simulacion/src/main/java/cantina/ucu/Implementaciones/Metricas.java
+++ simulacion/src/main/java/cantina/ucu/Implementaciones/Metricas.java
@@ -1,5 +1,6 @@
 package cantina.ucu.Implementaciones;
 
+import java.util.LinkedList;
 import java.util.Queue;
 
 import cantina.ucu.Interfaces.IPedido;
@@ -10,8 +11,24 @@ public class Metricas {
     private Queue<IPedido> pedidosSinAtender;
     private double promedioTiempoTotalPedidos;
     private double promedioTiempoEnEsperaPedidos;
+    private static Metricas metricas;
     
     //Hacer singleton
+    private Metricas() {
+        this.pedidosCompletados = new LinkedList<IPedido>();
+        this.tiempoCafeteraOcupada = 0;
+        this.pedidosSinAtender = new LinkedList<IPedido>();
+        this.promedioTiempoTotalPedidos = 0.0;
+        this.promedioTiempoEnEsperaPedidos = 0.0;
+    }
+
+    public static Metricas getMetricas() {
+        if (metricas == null) {
+            metricas = new Metricas();
+        }
+        return metricas;
+    }
+
     // Hacer metodos de metricas
     
     
